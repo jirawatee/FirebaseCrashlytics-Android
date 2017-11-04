e499b391603312dc43111eb73de59db460650a6a
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2017-11-04 11:12:13 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Firebase Crashlytics
+Firebase Crashlytics code guideline for Android developers
+
+## Prerequisites
+* Supported Android 4.0.1 (Ice Cream Sandwich) or newer
+* Google Play services 11.4.2 or newer
+* Android Studio 2.3 or higher
+* google-services.json in app-level folder
+
+## Features
+* Add custom event logs without logcat
+* Add custom event logs with logcat
+* Add keys/values
+* Set user info
+* Log exception
+
+## Screenshots
+<table width="100%">
+	<tr>
+	  <th><img src="https://user-images.githubusercontent.com/1763410/32402146-0f57d37a-c150-11e7-871a-a4384d95b735.png" width="100%"></th>
+	</tr>
+</table>
+
+## Blog
+[Firebase Crashlyics](https://medium.com/@jirawatee/%E0%B8%A3%E0%B8%B9%E0%B9%89%E0%B8%88%E0%B8%B1%E0%B8%81-firebase-crashlytics-%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B9%81%E0%B8%95%E0%B9%88-zero-%E0%B8%88%E0%B8%99%E0%B9%80%E0%B8%9B%E0%B9%87%E0%B8%99-hero-76712e6507c4)
---
+++ app/src/main/java/com/example/crashlytics/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.crashlytics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bindWidget();
+
+		Fabric.with(this, new Crashlytics());
+		Crashlytics.log("Start logging!");
+
+		Crashlytics.setUserIdentifier("id-99999");
+		Crashlytics.setUserName("FirebaseThailand");
+		Crashlytics.setUserEmail("firebasethailand@gmail.com");
+
+		Crashlytics.setString("key1", "value1");
+		Crashlytics.setBool("key2", true);
+		Crashlytics.setDouble("key3", 99.99);
+		Crashlytics.setFloat("key4", 999.99f);
+		Crashlytics.setInt("key5", 999);
+		Crashlytics.setLong("key6", System.currentTimeMillis());
+
+	}
+
+	private void bindWidget() {
+		findViewById(R.id.btn_force).setOnClickListener(this);
+		findViewById(R.id.btn_exception).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.btn_force:
+				Crashlytics.log(Log.DEBUG, TAG, "Log some message before a crash happen");
+				Crashlytics.getInstance().crash();
+				break;
+			case R.id.btn_exception:
+				try {
+					throw new NullPointerException();
+				} catch (NullPointerException ex) {
+					Crashlytics.logException(ex);
+					Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+				}
+				break;
+		}
+	}
+}
